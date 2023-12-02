d87007c89c7327fe240de6f5a14414a22116f754
SvitlanaElli <svitlanaelli@gmail.com>
2023-12-02 23:14:16 +0200

    Add 1 iteration

--- js/fineList.js
+++ js/fineList.js
@@ -6,6 +6,8 @@ window.fineList = {
 //Ця зміна містить всі дані які в нас зберігаються у файлі data
 let DB = data.finesData;
 
+
+
 function searchFines(searchKey){
     /*
      Напишіть свій код тут!
@@ -18,10 +20,64 @@ function searchFines(searchKey){
      - Невірне паркування
      - Їзда у не тверезому стані
      */
-
-
-    return [
-        {номер: '001', тип: 'Перевищення швидкості', сума: 100, дата: '2023-01-15'}
-    ];
+    if(opredelenieSearchKey(searchKey)){
+        
+    if(validInput(searchKey)){
+        alert("Пусто!!!");
+        return DB;
+    }
+    //идем по объекту и ищем введенный номер
+    for(let i = 0; i < DB.length; i++){
+    if ( DB[i].номер == searchKey){
+        return [DB[i]];
+    }
 }
 
+alert("Не найденно!!!1");
+return DB;
+    }else{
+        if(validInput(searchKey)){
+            alert("Пусто!!!");
+            return DB;
+        }
+        let arr = new Array();
+         for(let i = 0; i < DB.length; i++){
+             if ( DB[i].тип.toLowerCase() == searchKey.toLowerCase()){
+                arr.push(DB[i]);
+             }
+         }
+        if (arr.length){
+            return arr;
+        }
+        alert("Не найденно!!!2");
+        return DB;
+    }
+    
+
+}
+
+//Эта функция должна определять что мы вводим в поля и соответсветнно определять ищим мы по номеру или по названию
+function opredelenieSearchKey(searchKey){
+    //пытаемся понять searchKey слово или число
+    if(Number(searchKey)+1){
+        return 1;
+    }else{
+        return 0;}
+    
+}
+
+function validInput(searchKey){
+    if(searchKey === ""){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+
+//console.log(searchFines("Невірне паркування"));
+
+
+// return [
+//     {номер: '001', тип: 'Поиск по буквам', сума: 100, дата: '2023-01-15'},
+// ];
